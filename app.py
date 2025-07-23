f71ea79388ee37e22538d5a5776f2cbdd0345f71
Louis Faye <169034657+blarflelsouf@users.noreply.github.com>
2025-07-23 22:02:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -199,6 +199,14 @@ min_pop = st.sidebar.number_input("Population minimale", min_value=0, value=1000
 n = st.sidebar.number_input("Nombre d'agglomérations à afficher", min_value=1, max_value=30, value=10)
 
 @st.cache_data
+def test_debug(x):
+    print("DEBUG APPEL FONCTION", x)
+    return x * 2
+
+st.title("Test debug print")
+val = st.number_input("Valeur", 1, 100, 10)
+result = test_debug(val)
+st.write(result)@st.cache_data
 def test_print(x):
     print("CALL API", x)
     return x*2
